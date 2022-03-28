3c93dd4a5ff61ce3093ca62419822f5ac4386fe9
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2022-03-28 23:36:59 +0900

    add solver_one_cube

---
+++ solver_one_cube/definition.hpp
@@ -0,0 +1,9 @@
+#define WHITE 0
+#define GREEN 1
+#define RED 2
+#define BLUE 3
+#define ORANGE 4
+#define YELLOW 5
+
+#define HW_CUBE 3
+#define HW2_CUBE 9
---
+++ solver_one_cube/solver_one_cube.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "definition.hpp"
+
+using namespace std;
+
+int main(int argc, char *argv[]){
+    int 
+    
+    return 0;
+}
